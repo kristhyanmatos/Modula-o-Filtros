e5d57e31f56bd47bf7cf29fcdc06b3bb5a5c77a4
kristhyanmatos <kristhyandematosmaia@gmail.com>
2019-12-15 12:52:46 -0300

    att

--- pds/receptor.m
+++ pds/receptor.m
@@ -1,40 +1,47 @@
+# ETAPA 1
 # Carrega o sinal de mensagem
 [sinal, fa] = audioread('C:\Users\ssoar\Documents\GitHub\Modula-o-Filtros\pds\audio.wav');
 
-# filtro passa faixa
-# n ordem [intervalo min e max]
+# ETAPA 2
+# Filtro passa faixa
+# N ordem [intervalo min e max]
 h1 = fir1(100, [0.18 0.42]);
 
-# convolução entre os sinais
-filtradoPF = filter(h1,1,sinal);
-# demodulação
-Vc(1) = 0;                              % initial capacitor voltage
+# Convolução entre os sinais
+filtradoPF = conv(h1,sinal);
+
+# ETAPA 3
+# Demodulação
+sinalDemodulado(1) = 0;                              
 for i = 2:length(filtradoPF)
-    if filtradoPF(i) > Vc(i-1)                   % diode on (charging)
-        Vc(i) = filtradoPF(i);
-    else                                % diode off (discharging)
-        Vc(i) = Vc(i-1) - 0.023*Vc(i-1);
+    if filtradoPF(i) > sinalDemodulado(i-1)
+        sinalDemodulado(i) = filtradoPF(i);
+    else
+        sinalDemodulado(i) = sinalDemodulado(i-1) - 0.023*sinalDemodulado(i-1);
     end
 end
 
-# filtro passa faixa voz
+# ETAPA 4
+# Filtro passa faixa voz
 hv = fir1(200, [0.025 0.13]);
-filtradoVoz = filter(hv,1,Vc);
+filtradoVoz = conv(hv,sinalDemodulado);
 
-# filtro passa baixa
-fp = 4500;  # frenquencia de passagem
-fc = 5000; # frequencia de corte
+# ETAPA 5
+# Filtro passa baixa
+fp = 1500;  # Frenquencia de passagem
+fc = 1800;  # Frequencia de corte
 
-# normalização das frequencias
+# Normalização das frequencias
 wp = (fp/(fa/2))*pi;
 wc = (fc/(fa/2))*pi;
 
-wt = wc - wp;   # frenquencia de transferencia
-wci = (wc+wp)/2; # frenquencia de corte intermediaria
+wt = wc - wp;   # Frenquencia de transferencia
+wci = (wc+wp)/2; # Frenquencia de corte intermediaria
 
 M = ceil((6.6*pi/wt)) + 1;
 hd = passaBaixaideal(wci,M);
 w_ham = hamming(M)';
 h2 = hd.*w_ham;
 
-sinalPassado = conv(h2,filtradoVoz); 
+# Convolução entre os sinais
+sinalPassado = conv(h2,filtradoVoz);
