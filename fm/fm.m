3e59d8a59a4b9e3506540d9e6bb60ab2e22a0ea7
kristhyanmatos <kristhyandematosmaia@gmail.com>
2019-12-16 20:28:25 -0300

    foi mais um

--- fm/fm.m
+++ fm/fm.m
@@ -1,22 +1,21 @@
-# formula conceitual
-#  A0cos(w0+Bmt);
 fa = 1E6;                             # Frequência de amostragem
-t  = [0:1/fa:1];                      # Periodo de amostragem
-kf = 1000;                              # Fator de sensibilidade de frequência
+t  = [0:1/fa:0.1];                    # Periodo de amostragem
+kf = 1000;                            # Fator de sensibilidade de frequência
+ap = 1;                               # Amplitude da portadora
+am = 5;                               # Amplitude do sinal
 fp = 1000;                            # Frequência da portadora
-fm = 300;                             # Frequência do sinal mt
+fm = 50;                              # Frequência do sinal mt
 wc = 2*pi*fp;                         # Normalização da frequência da portadora
 wm = 2*pi*fm;                         # Normalização da frequência do sinal mt
-b  = (kf)/wm;                         # Índice de modulação
+b  = (kf*am)/wm;                      # Índice de modulação
 y  = cos(wc*t);                       # Portadora
-mt = cos(wm*t);                       # Sinal
+mt = sin(wm*t);                       # Sinal
 
-sinalModuladoFm = cos(wc*t + b*mt);                 # Formula conceitual
-
-
+sinalModuladoFm = ap*cos(wc*t + b*mt);  # Formula conceitual
 
 plot(t,y); title('portadora');
 figure();
 plot(t,mt); title('sinal');
 figure();
 plot(t,sinalModuladoFm); title('Modulação em FM');
+trf(sinalModuladoFm,fa);
